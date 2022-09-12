e7f0f8468789f7ad6e290ba36918101f82c6cf83
Timotej Petko <petkotimotej@gmail.com>
2022-09-12 12:04:35 +0200

    riesenie v2.

--- zadanie1/Petko-4A-uloha.py
+++ zadanie1/Petko-4A-uloha.py
@@ -1,36 +1,37 @@
 zoznam_cisel=[]
-pomocna_premenna = 0
 while True:
-    print('Ak chces program ukoncit, zadaj do vstupu string')
-    cislo=input('Zadaj integer: ')
-    if cislo.isdigit():
-        for i in range(len(zoznam_cisel)):
-            if zoznam_cisel[i][0] == cislo:
-                pomocna_premenna = 1
-                zoznam_cisel[i]=list(zoznam_cisel[i])
-                zoznam_cisel[i][1]+=1
-                zoznam_cisel[i]=tuple(zoznam_cisel[i])
+    pomocna_premenna = 0
+    cislo=int(input('Zadaj integer: '))
+    for i in range(len(zoznam_cisel)):
+        if zoznam_cisel[i][0] == cislo:
+            pomocna_premenna = 1
+            zoznam_cisel[i][1] += 1
 
-        if pomocna_premenna == 0:        
-            zoznam_cisel.append((cislo,1))
-        pomocna_premenna = 0
+    if pomocna_premenna == 0:        
+        zoznam_cisel.append([cislo,1])
 
-        for i in range(0,len(zoznam_cisel)-1):  
-            for j in range(len(zoznam_cisel)-1):  
-                if zoznam_cisel[j][0]>zoznam_cisel[j+1][0]:
-                    zoznam_cisel[j] = list(zoznam_cisel[j])
-                    zoznam_cisel[j+1] = list(zoznam_cisel[j+1])
-                    temp = zoznam_cisel[j][0]  
-                    zoznam_cisel[j][0] = zoznam_cisel[j+1][0]  
-                    zoznam_cisel[j+1][0] = temp
-                    zoznam_cisel[j] = tuple(zoznam_cisel[j])
-                    zoznam_cisel[j+1] = tuple(zoznam_cisel[j+1]) 
-    else:
-        print('data boli ulozene v subore')
-        subor=open('data','w')
-        for i in range(len(zoznam_cisel)):
-            subor.write(str(('cislo',zoznam_cisel[i][0],'bolo zadane',zoznam_cisel[i][1],'krat')))
-        subor.write('\n')
-        subor.write(str(('tri najvacsie cisla ',zoznam_cisel[-3][0],',',zoznam_cisel[-2][0],',',zoznam_cisel[-1][0],'s poctom vyskytov: ',zoznam_cisel[-3][1],',',zoznam_cisel[-2][1],',',zoznam_cisel[-1][1])))
-        break
+    for i in range(len(zoznam_cisel)-1,0,-1):  
+        for j in range(i):  
+             if zoznam_cisel[j][0] > zoznam_cisel[j+1][0]:
+                zoznam_cisel[j] , zoznam_cisel[j+1] = zoznam_cisel[j+1] , zoznam_cisel[j]            
     
+    subor=open('status','w')
+    subor.write('Zadane cisla: ')
+    for i in range(len(zoznam_cisel)):
+        subor.write(str(zoznam_cisel[i][0])+'('+str(zoznam_cisel[i][1])+'x), ')
+    if len(zoznam_cisel) > 2:
+        for i in range(len(zoznam_cisel)-1,0,-1):  
+            for j in range(i):
+                if zoznam_cisel[j][1] > zoznam_cisel[j+1][1]:
+                    zoznam_cisel[j] , zoznam_cisel[j+1] = zoznam_cisel[j+1] , zoznam_cisel[j]
+        subor.write('\n')
+        subor.write('Tri cisla s najvacsim poctom vyskytov: ')
+        for i in range(1,4):
+            subor.write(str(zoznam_cisel[-i][0])+'('+str(zoznam_cisel[-i][1])+'x), ')
+
+    else:
+        subor.write('\n')
+        subor.write('Neboli zadane 3 rozne cisla')
+
+    print('data boli ulozene do suboru :)')
+    subor.close()    
