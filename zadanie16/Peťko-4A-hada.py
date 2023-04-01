c324f62834885f5e20f833e1849e4a8ea2eae122
trimotoj <petkotimotej@gmail.com>
2023-04-01 18:58:24 +0200

    updated solution

--- zadanie16/Peťko-4A-hada.py
+++ zadanie16/Peťko-4A-hada.py
@@ -1,23 +1,19 @@
-pocet_hier=0
-najdlhsia_hra=0
-
-with open('zadanie16/hada.txt','r') as file:
-    komprim=open('zadanie16/komprim.txt','w')
+games=0
+lenght=0
+file2=open('zadanie16/hada2','w')
+with open('zadanie16/hada','r') as file:
     for line in file:
         line=line.strip()
-        hra=pocet=1
-        for i in range(1,len(line)):
-            if line[i] == line[i-1]:
+        games+=1
+        pocet=1
+        if len(line) > lenght:
+            lenght= len(line)
+        for i in range(len(line)-1):
+            if line[i+1] == line[i]:
                 pocet+=1
             else:
-                komprim.write(line[i-1]+str(pocet)+' ')
-                pocet = 1
-            hra+=1
-        if najdlhsia_hra < hra:
-            najdlhsia_hra = hra
-        pocet_hier+=1
-        komprim.write(line[i]+str(pocet))
-        komprim.write('\n')
-
-print('pocet hier:',pocet_hier)
-print('najdlhsia hra:',najdlhsia_hra)
+                file2.write(line[i]+' '+str(pocet)+' ')
+                pocet=1
+        file2.write(line[i+1]+' '+str(pocet)+'\n')
+print('Pocet hier:',games)
+print('Najdlhsia hra:',lenght)
